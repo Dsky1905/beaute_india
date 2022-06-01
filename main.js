7594e93e8936b05e0610583feb55571887a7251b
Darshan1905 <dpprajapati1905@gmail.com>
2022-06-01 10:01:24 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,18 @@
+
+// For Productpage toggle
+
+function myFunction() {
+  var element = document.getElementById("myDIV");
+  element.classList.toggle("open");
+}
+
+
+
+// For Mobile Navigation toggle
+function openside() {
+  var element = document.getElementById("side");
+  element.classList.toggle("on");
+}
+  
+
+
